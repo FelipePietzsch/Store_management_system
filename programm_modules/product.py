395bf774d013ad6ecdc5eec330b7c3ef48445195
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-01-28 10:33:03 +0100

    promotions implemented in Products class

--- programm_modules/product.py
+++ programm_modules/product.py
@@ -1,4 +1,5 @@
 from math import inf
+from programm_modules import promotion
 
 class Product:
   """Contains all data and functions, to interact with a specific type of product"""
@@ -7,6 +8,7 @@ class Product:
     self.price = price
     self.quantity = quantity
     self.active = active
+    self.promotion = None
 
 
     # self.name exceptions
@@ -45,10 +47,6 @@ class Product:
       raise ValueError("Error: Quantity must not be negative")
 
 
-  # hier eibauen, dass man nur quantity bentutzen muss
-  # 1. self.quanitity() = x
-  # 2. print(self.quantitiy())
-
   def get_quantity(self) -> int:
     """returns the quantity of the Product instance"""
     return self.quantity
@@ -84,8 +82,12 @@ class Product:
     """executes a 'buy' action for the product. checks if given quanity is valid calculates new quanity of the product"""
     total_price = self.price * quantity
 
+    # reduces price by promotion
+    if isinstance(self.promotion, promotion.Promotion):
+      total_price = self.promotion.apply_promotion(quantity)
+
     if self.quantity < quantity:
-      # raise will be risen by store.py:38
+      # exception will be risen by store.py:38
       raise ValueError(f"Not enough {self.name}´s in stock. Only {self.quantity} {self.name}´s left.")
     else:
       self.quantity -= quantity
@@ -93,6 +95,17 @@ class Product:
     if self.quantity == 0:
       self.active = False
       return f"Total Price: {total_price}, new product quantiy: {self.quantity}"
+
+
+  @property
+  def promotion(self):
+    return self.promotion
+
+
+  @promotion.setter
+  def promotion(self, promotion):
+    self.promotion = promotion
+
 
 
 class DigitalProduct(Product):
